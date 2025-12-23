ffff02ec94d6baf0573cbfb76acb051e4c05e280
Yearis <khush8751@gmail.com>
2025-12-23 21:19:05 +0530

    "Simple Bug fix"

--- src/main/java/com/yearis/e_commerce/repository/cart/CartRepository.java
+++ src/main/java/com/yearis/e_commerce/repository/cart/CartRepository.java
@@ -1,7 +1,6 @@
 package com.yearis.e_commerce.repository.cart;
 
 import com.yearis.e_commerce.entity.Cart;
-import com.yearis.e_commerce.payload.cart.CartResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
